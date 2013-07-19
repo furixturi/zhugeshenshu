6dd0c6070e1da51a9c193da4563cb0665338f9e2
Xiaoli Shen <alabebop@gmail.com>
2013-07-19 14:21:35 +0200

    html structure and bootstrap

---
+++ js/script.js
@@ -0,0 +1,29 @@
+(function($, win){
+    var guaCoins = [0, 0, 0, 0, 0, 0, 0, 1];
+    var numberCoins = [0, 0, 0, 0, 0, 1];
+
+    function getNext(arr){
+        return arr.pop() === 1;
+    }
+
+    /**
+     * Fisher-Yates Shuffle / Knuth Shuffle
+     * courtesy: http://www.htmlblog.us/random-javascript-array
+     *
+     */
+    function shuffleArr(arr){
+        var shuffeled = arr ? arr.slice(0) : [], i = shuffeled.length, j, temp;
+        while(--i){
+            j = Math.floor(Math.random()*(i-1));
+            temp = shuffeled[i];
+            shuffeled[i] = shuffeled[j];
+            shuffeled[j] = temp;
+        }
+        return shuffeled;
+    }
+    
+    
+	$(win.document).ready(function(){
+		
+	});    
+})(jQuery, window);
