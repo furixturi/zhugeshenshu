eb1470f9d470487bbb3038edd3283431537ed540
Xiaoli Shen <alabebop@gmail.com>
2013-07-19 12:07:55 +0200

    helper functions

---
+++ script.js
@@ -0,0 +1,18 @@
+var guaCoins = [0, 0, 0, 0, 0, 0, 0, 1];
+var numberCoins = [0, 0, 0, 0, 0, 1];
+
+function getNext(arr){
+	return arr.pop() === 1;
+}
+
+function shuffleArr(arr){
+	var shuffeled = arr ? arr.slice(0) : [], i = shuffeled.length, j, temp;
+	while(--i){
+		j = Math.floor(Math.random()*(i-1));
+		temp = shuffeled[i];
+		shuffeled[i] = shuffeled[j];
+		shuffeled[j] = temp;
+	}
+	return shuffeled;
+}
+
