ea0c0e12bec799180d5f4d5958fc4d91dd926b3d
furixturi <alabebop@gmail.com>
2019-03-02 21:13:24 +0900

    parse data

--- parse-data/dist/index.js
+++ parse-data/dist/index.js
@@ -1,3 +1,47 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('hi');
+var cheerio = __importStar(require("cheerio"));
+var fs = __importStar(require("fs"));
+var html = fs.readFileSync(__dirname +
+    '/../data.html', 'utf8');
+var $ = cheerio.load(html);
+var started = false;
+var counter = 0;
+var qian;
+var qians = [];
+$('.al_txt p').each(function (i, elm) {
+    var text = $(elm).text();
+    if (text === '秘本诸葛神数384签签文') {
+        started = true;
+        return true;
+    }
+    if (started) {
+        if (text.indexOf('第') === 0) {
+            counter++;
+            qian = {
+                indexText: text,
+                index: counter
+            };
+            qians.push(qian);
+        }
+        else if (text.indexOf('签诗') === 0) {
+            qian.qian = text;
+        }
+        else if (text.indexOf('解签') === 0) {
+            qian.explanation = text;
+        }
+        else {
+            qian.explanation += text;
+        }
+    }
+});
+fs.writeFileSync('./data.js', 'const qians = ' +
+    JSON.stringify(qians, null, 2) + ';');
+//# sourceMappingURL=index.js.map
--- parse-data/src/index.ts
+++ parse-data/src/index.ts
@@ -1,7 +1,48 @@
-import { parse } from 'node-html-parser';
+import * as cheerio from 'cheerio';
 import * as fs from 'fs';
 
-console.log('=== Parse html ===');
+interface Qian {
+  indexText: string;
+  index: number;
+  qian?: string;
+  explanation?: string;
+}
 
-const html = fs.readFileSync(__dirname + '/../data.htm', 'utf8');
-console.log(html)
+const html:string = fs.readFileSync(__dirname + 
+  '/../data.html', 'utf8');
+
+const $: CheerioStatic = cheerio.load(html);
+
+let started = false;
+let counter = 0;
+let qian: Qian;
+const qians: Array<Qian> = [];
+
+$('.al_txt p').each((i, elm) => {
+  const text = $(elm).text();
+
+  if(text === '秘本诸葛神数384签签文') {
+    started = true;
+    return true;
+  }
+
+  if(started) {
+    if(text.indexOf('第') === 0) {
+      counter++;
+      qian = {
+        indexText: text,
+        index: counter
+      };
+      qians.push(qian);
+    } else if(text.indexOf('签诗') === 0) {
+      qian.qian = text;
+    } else if(text.indexOf('解签') === 0) {
+      qian.explanation = text;
+    } else {
+      qian.explanation += text;
+    }
+  }
+  
+});
+fs.writeFileSync('./data.js', 'const qians = ' + 
+  JSON.stringify(qians, null, 2) + ';');
