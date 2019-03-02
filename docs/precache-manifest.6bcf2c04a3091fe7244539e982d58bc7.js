6043a6943456bcdd3552acb23290c1a111b79758
furixturi <alabebop@gmail.com>
2019-03-03 01:08:11 +0900

    update build script to remove docs folder first, add homepage field in package.json

---
+++ docs/precache-manifest.6bcf2c04a3091fe7244539e982d58bc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b1988d0fd6870667150",
+    "url": "./static/css/main.e30aee8e.chunk.css"
+  },
+  {
+    "revision": "3b1988d0fd6870667150",
+    "url": "./static/js/main.3b1988d0.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "./static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "742657236511884dc577ba0158075095",
+    "url": "./index.html"
+  }
+];
