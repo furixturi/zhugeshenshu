7e4ad744d287ca2f578fc9dee77db251fa023511
furixturi <alabebop@gmail.com>
2019-04-14 10:53:29 +0900

    add more info model interface

--- models/index.ts
+++ models/index.ts
@@ -3,12 +3,18 @@ export interface SequenceItem {
   yao: string;
 }
 
+export interface MoreInfo {
+  shortInfo: string;
+  longInfo: Array<string>;
+}
+
 export interface Qian {
   explanation: string;
   index: number;
   indexText: string;
   qian: string;
   yao: string;
+  moreInfo: MoreInfo;
 }
 
 export interface QianRaw {
