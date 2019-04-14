d3cc280deec800706ee7a5aa23b6a79369f6bc7d
furixturi <alabebop@gmail.com>
2019-04-14 10:55:34 +0900

    describe sequence generation logic in comment

--- parse-data/src/sequence.ts
+++ parse-data/src/sequence.ts
@@ -16,6 +16,16 @@ const buildSequenceArray = () : Array<SequenceItem> => {
 
   let counter = 1;
 
+  /**
+   * The wheel pair is starting at the current gua, counting forwards 4 guas,
+   * then counting bakcwards 4 guas from the gua behind the current gua,
+   * rotating back to go through the beginning if the counting goes
+   * further than all the guas.
+   * E.g.:
+   * If current gua is 兑, the corresponding yao wheel will be:
+   * 兑兑, 兑离, 兑震, 兑坤
+   * 兑乾, 兑巽, 兑坎, 兑艮
+   */
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       let o =
